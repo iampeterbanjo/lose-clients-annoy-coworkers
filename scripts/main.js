18ca15480b1e7c14c1e76c758364d56dc9de69a3
iampeterbanjo <peterbanjo@gmail.com>
2016-01-15 09:42:36 +0000

    Refactor App to remove function name duplication

--- scripts/main.js
+++ scripts/main.js
@@ -3,78 +3,72 @@ console.log('ready');
 var App = App || function() {
 	this.previousOffense = -1;
 	
-	// get a url and call success or error functions
-	function get(url, success, error) {
-		var request = new XMLHttpRequest();
-		request.open('GET', url, true);
+	return {
+		// get a url and call success or error functions
+		get: function(url, success, error) {
+			var request = new XMLHttpRequest();
+			request.open('GET', url, true);
 
-		request.onload = function() {
-			if(request.status >= 200 && request.status < 400) {
-				success(request.responseText);
+			request.onload = function() {
+				if(request.status >= 200 && request.status < 400) {
+					success(request.responseText);
+				}
+			}
+
+			request.onerror = function() {
+				error(request.status, request.responseText);
+			}
+
+			request.send();
+		}
+		// return random integer between range
+		, getRandomInt: function(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+		// set state of the app
+		,  changeState: function(newState) {
+			var body = document.querySelector('body'),
+					state = newState;
+
+			switch (state) {
+				case 'LOADING':
+					body.classList.add('loading');
+					body.classList.remove('loaded');
+					break;
+				case 'DONE':
+					body.classList.remove('loading');
+					body.classList.add('loaded');
+					break;
+				case 'SUGGESTION':
+					body.classList.add('suggestion');
+					body.classList.remove('close-suggestion');
+					break;
+				case 'CLOSE-SUGGESTION':
+					body.classList.remove('suggestion');
+					body.classList.add('close-suggestion');
+					break;
 			}
 		}
-
-		request.onerror = function() {
-			error(request.status, request.responseText);
-		}
-
-		request.send();
-	}
-	
-	// return random integer between range
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-	
-	// set state of the app
-	function changeState(newState) {
-		var body = document.querySelector('body'),
-				state = newState;
-
-		switch (state) {
-			case 'LOADING':
-				body.classList.add('loading');
-				body.classList.remove('loaded');
-				break;
-			case 'DONE':
-				body.classList.remove('loading');
-				body.classList.add('loaded');
-				break;
-			case 'SUGGESTION':
-				body.classList.add('suggestion');
-				body.classList.remove('close-suggestion');
-				break;
-			case 'CLOSE-SUGGESTION':
-				body.classList.remove('suggestion');
-				body.classList.add('close-suggestion');
-				break;
-		}
-	}
-	
-	// make new request for data and show it
-	function reloadData() {
-		changeState('LOADING');
-		
-		get('data/how-to.json',
-		function(text){
-			changeState('DONE');
+		// make new request for data and show it
+		, reloadData: function() {
+			var self = this;
 			
-			var data = JSON.parse(text),
-					offense = document.getElementById('offense'),
-					random = data[getRandomInt(0, data.length -1)],
-					description = random.description !== '' ? random.description : '';
+			self.changeState('LOADING');
 			
-			localStorage['last'] = description;
-					
-			offense.innerHTML = description;
-		});
-	}
-	
-	return {
-		get: get
-		, getRandomInt: getRandomInt
-		,  changeState: changeState
-		, reloadData: reloadData
+			self.get('data/how-to.json',
+			function(text){
+				self.changeState('DONE');
+				
+				var data = JSON.parse(text),
+						offense = document.getElementById('offense'),
+						random = data[self.getRandomInt(0, data.length -1)],
+						description = random.description !== '' ? random.description : '';
+				
+				localStorage['last'] = description;
+						
+				offense.innerHTML = description;
+			});
+		}
 		, getNext: function() {}
 	}
 }
