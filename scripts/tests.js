f829332731d195ce748d949fa8d60133f11d0fd0
iampeterbanjo <peterbanjo@gmail.com>
2016-01-15 09:48:20 +0000

    Refactor Test harness

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,20 +1,25 @@
 // tests
-
-function assert(test, description) {
-	var status = !!test ? 'PASSED' : 'FAILED';
+var Test = Test || function() {
+	return {
+		assert: function(test, description) {
+			var status = !!test ? 'PASSED' : 'FAILED';
 	
-	console[ !!test ? 'info': 'warn' ]('%s : %s', status, description);
-}
-
-assert(true, 'sanity check');
-
-function testAppHasGetNextFunction() {
-	var app = new App();
-	assert(typeof app.getNext == 'function', 'app has getNext function');
+			console[ !!test ? 'info': 'warn' ]('%s : %s', status, description);
+		}
+		, appHasGetNextFunction: function() {
+			var app = new App();
+			
+			this.assert(typeof app.getNext == 'function', 'app has getNext function');
+		}
+	}
 }
 
 if ((window.outerHeight - window.innerHeight) > 100) {
-	console.log('Docked inspector open - running tests');
+	console.log('** Docked inspector open >> running tests << **');
 	
-	testAppHasGetNextFunction();
+	var test = new Test();
+	
+	test.assert(true, 'sanity check');
+	
+	test.appHasGetNextFunction();
 }
